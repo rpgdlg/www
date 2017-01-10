38275a89e174a3d518a3c971c9956ed46b94755e
rpgdlg <rpgdlg9@gmail.com>
2017-01-10 01:26:40 -0600

    v0.01
    primera version

---
+++ core/core.php
@@ -0,0 +1,10 @@
+<?php
+
+  /*  ARCHIVO DE CONFIGURACION  */
+
+  define('HTML_DIR','html/');
+  define('APP_TITLE', 'Benevento');
+  define('APP_COPY', 'Copyright &copy; ' . date('Y', time()) . ' Daniel García Landeros Software. ');
+
+  require('vendor/autoload.php');
+ ?>
